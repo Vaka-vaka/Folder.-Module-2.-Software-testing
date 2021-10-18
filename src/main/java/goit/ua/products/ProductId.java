9a5eb69d73bf2c1f0963cfb6ec20a848b6653ec0
Vaka-vaka <Waka2006@ukr.net>
2021-10-19 00:29:49 +0300

    Create ProductId.java

---
+++ src/main/java/goit/ua/products/ProductId.java
@@ -0,0 +1,12 @@
+/**
+ * Homework. Module 2. jUnitTest
+ *
+ *  @autor Valentin Mozul
+ *  @version of 18.10.2021
+ */
+
+package goit.ua.products;
+
+public interface ProductId<ID> {
+    ID getId();
+}
