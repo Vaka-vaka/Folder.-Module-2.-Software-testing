56943b8076a9a49f5bb359e4320553e3ce41ec48
Vaka-vaka <Waka2006@ukr.net>
2021-10-18 23:43:59 +0300

    Create UserTest.java

---
+++ src/test/java/goit/ua/UserTest.java
@@ -0,0 +1,21 @@
+package goit.ua;
+
+import org.junit.*;
+
+/**
+ * Homework. Module 2. jUnitTest
+ *
+ *  @autor Valentin Mozul
+ *  @version of 18.10.2021
+ */
+
+
+public class UserTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+
+    }
+}
