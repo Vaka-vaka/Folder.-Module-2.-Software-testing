b4fd1285581ec2eab81efb7f230b745d047f55a0
Vaka-vaka <Waka2006@ukr.net>
2021-10-18 23:53:41 +0300

    Create ProductTest.java

---
+++ src/test/java/goit/ua/ProductTest.java
@@ -0,0 +1,20 @@
+/**
+ * Homework. Module 2. jUnitTest
+ *
+ *  @autor Valentin Mozul
+ *  @version of 18.10.2021
+ */
+
+package goit.ua;
+
+import org.junit.*;
+
+public class ProductTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+
+    }
+}
