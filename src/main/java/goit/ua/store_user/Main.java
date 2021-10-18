fb47a259c591f672cd36fc22764405eca0ed3c50
Vaka-vaka <Waka2006@ukr.net>
2021-10-19 02:09:10 +0300

    Create Main.java

---
+++ src/main/java/goit/ua/store_user/Main.java
@@ -0,0 +1,19 @@
+/**
+ * Homework. Module 2. jUnitTest
+ *
+ * @autor Valentin Mozul
+ * @version of 18.10.2021
+ */
+
+package goit.ua.store_user;
+
+import goit.ua.services.ProductServices;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ProductServices productServices = new ProductServices();
+        double basket = productServices.calculateTheCostOfTheBasket("ABCDABA");
+        System.out.println(basket);
+    }
+}
