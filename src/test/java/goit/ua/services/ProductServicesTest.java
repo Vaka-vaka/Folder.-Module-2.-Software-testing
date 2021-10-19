9579d4fcf3fae7935b9bdeaa1de424998ce3206e
Vaka-vaka <Waka2006@ukr.net>
2021-10-19 23:25:04 +0300

    Update ProductServicesTest.java

--- src/test/java/goit/ua/services/ProductServicesTest.java
+++ src/test/java/goit/ua/services/ProductServicesTest.java
@@ -2,10 +2,14 @@
  * Homework. Module 2. jUnitTest
  *
  * @autor Valentin Mozul
- * @version of 18.10.2021
+ * @version of 19.10.2021
  */
 
 package goit.ua.services;
 
 public class ProductServicesTest {
+    /**
+     * Rigorous Test :-)
+     */
+
 }
