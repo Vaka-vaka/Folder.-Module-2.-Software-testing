44e944fe1d39d4fe8803b6fe331b7101b2c17797
Vaka-vaka <Waka2006@ukr.net>
2021-10-28 16:32:23 +0300

    Update Main.java

--- src/main/java/goit/ua/store_user/Main.java
+++ src/main/java/goit/ua/store_user/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
     public static void main(String[] args) {
         ProductServices productServices = new ProductServices();
-        double basket = productServices.calculateTheCostOfTheBasket("ABCDABA");
+        double basket = productServices.calculateCostOfBasket("ABCDABA");
         System.out.println(basket);
     }
 }
